7c6cb1fc0453c6f89bc6da43e18e8f111ab6726f
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-07-05 19:57:16 +0530

    Added Project React Lyrics App

---
+++ precache-manifest.eaac73d8dc97704f90568d657a8fddc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce89bf5139f178c08080bcb69b21c47e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d69f5b95a364916d23",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fe789a343e140c5f6a12",
+    "url": "/static/js/2.cf78d365.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.cf78d365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d69f5b95a364916d23",
+    "url": "/static/js/main.d2d8e1f0.chunk.js"
+  },
+  {
+    "revision": "d95da301c6277ab50d9a",
+    "url": "/static/js/runtime-main.6a5704ef.js"
+  }
+]);
